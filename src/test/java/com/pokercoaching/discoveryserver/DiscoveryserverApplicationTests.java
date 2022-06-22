e091917f8bd6e4db02ac33377c8a305cac6a8b61
salallegra <salvatore.allegra@smoothstack.com>
2022-06-21 22:23:27 -0700

    Initial Commit

---
+++ src/test/java/com/pokercoaching/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokercoaching.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
